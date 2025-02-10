ce1c93df4d9765f784f83c15849572d1e830f909
Pedro Pardal <ppardalj@gmail.com>
2025-02-10 17:52:36 +0100

    first commit

---
+++ src/test/java/com/exeal/blue_green_demo/BlueGreenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exeal.blue_green_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueGreenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
